9e6a29a70fbff365b0540f33db89539e76b6b025
Dima <dimahelper@mail.ru>
2019-07-14 00:18:24 +0300

    l3_m1_Опции командной строки

--- src/test/java/ru/stqa/training/selenium/LitecartLogin.java
+++ src/test/java/ru/stqa/training/selenium/LitecartLogin.java
@@ -4,9 +4,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LitecartLogin {
@@ -15,20 +20,30 @@ public class LitecartLogin {
 
   @Before
   public void start() {
-    driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    //конструирование объекта (инициализация драйвера и браузера)
+    ChromeOptions options = new ChromeOptions(); //объект опций командной строки
+    options.addArguments("start-fullscreen");
+//    driver = new ChromeDriver(options);
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability(ChromeOptions.CAPABILITY, options);
+    driver = new ChromeDriver(caps);
+    /*DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+    driver = new InternetExplorerDriver(caps);*/
+
+    System.out.println(((HasCapabilities) driver).getCapabilities());
+//  driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
-  public void litecartLoginTest() {
+  public void litecartLoginTest() throws InterruptedException {
     driver.get("http://localhost/litecart/admin/");
     driver.findElement(By.name("username")).sendKeys("admin");
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("login")).click();
-
+    Thread.sleep(4000);
   }
-  
+
   @After
   public void stop() {
     driver.quit();
